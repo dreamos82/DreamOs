63bb67de8724b5b78a4f417c892dc92a66f94c25
Ivan Gualandri <ivan.gualandri@gmail.com>
2009-02-01 22:34:30 +0000

    Corretti gli include in shell.c E aggiunte le informazioni di licenza.

--- shell/shell.c
+++ shell/shell.c
@@ -1,4 +1,43 @@
+/*
+ * Dreamos
+ * shell.c
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #include <commands.h>
+#include <multiboot.h>
+#include <kernel.h>
+#include <stddef.h>
+#include <video.h>
+#include <pic8259.h>
+#include <8253.h>
+#include <gdt.h>
+#include <idt.h>
+#include <cpuid.h>
+#include <stdio.h>
+#include <string.h>
+#include <fismem.h>
+#include <io.h>
+#include <keyboard.h>
+#include <paging.h>
+#include <use.h>
+#include <shell.h>
+#include <version.h>
+#include <cpuid.h>
+#include <clock.h>
+#include <sys/utsname.h>
 
 #define NUM_COM 14
 
