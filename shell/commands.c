223c6c27d41af30c3f93a7de12cda32e131e23b9
Ivan Gualandri <ivan.gualandri@gmail.com>
2010-06-02 21:11:17 +0000

    Aggiunto comando newfile, per creare un file di testo. Per ora e' limitato ad una sola riga di input, di dimensione massima di 256 caratteri.

--- shell/commands.c
+++ shell/commands.c
@@ -550,5 +550,20 @@ void newfile(){
 		if(strcmp(argv[1], "--help")==0) {
 			printf("newfile FILENAME - Make a new file, and prompt for it's content\n");
 		}
+		else {
+			int fd;			
+			fd = open(argv[1], O_RDONLY);
+			if(fd>=0) printf("-ERROR: %s File already exist\n", argv[1]);
+			else {
+				char text[256];
+				printf("Filname: %s", argv[1]);
+				close(fd);
+				fd = open(argv[1], O_RDWR|O_CREAT|O_APPEND);
+				printf("Type your text here, actually only one line available!!\n");
+				scanf("%s", text);
+				if(fd>=0) write(fd,text, strlen(text));
+				close(fd);
+			}
+		}
 	}
 }
