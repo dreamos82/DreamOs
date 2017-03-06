2134f08a560e711bdf69cbf010da80c8c3011e48
Galfurian <enry.frak@gmail.com>
2017-03-06 18:30:16 +0100

    Clean up the code of atomic operations and spinlock

--- src/dreamos/inc/libc/spinlock.h
+++ src/dreamos/inc/libc/spinlock.h
@@ -21,74 +21,6 @@ typedef struct spinlock_t
     uint32_t counter;
 } spinlock_t;
 
-inline static bool_t spinlock_init(spinlock_t * spinlock)
-{
-    if (!spinlock)
-    {
-        return false;
-    }
-    dbg_print("Initialize spinlock...\n");
-    atomic_set(&spinlock->queue, 0);
-    atomic_set(&spinlock->dequeue, 1);
-    spinlock->owner = MAX_THREADS;
-    spinlock->counter = 0;
-    return true;
-}
-
-inline static bool_t spinlock_destroy(spinlock_t * spinlock)
-{
-    if (!spinlock)
-    {
-        return false;
-    }
-    spinlock->owner = MAX_THREADS;
-    spinlock->counter = 0;
-    return true;
-}
-
-
-inline static bool_t spinlock_lock(spinlock_t * spinlock)
-{
-    if (!spinlock)
-    {
-        return false;
-    }
-    thread_t * current_thread = kernel_get_current_thread();
-    dbg_print("[%d] Trying to lock...\n", current_thread->id);
-    int32_t ticket;
-    if (spinlock->owner == current_thread->id)
-    {
-        spinlock->counter++;
-        return true;
-    }
-    ticket = atomic_add(&spinlock->queue, 1);
-    while (atomic_read(&spinlock->dequeue) != ticket)
-    {
-        __asm__("nop");
-    }
-    spinlock->owner = current_thread->id;
-    spinlock->counter = 1;
-    dbg_print("[%d] Acquired lock...\n", current_thread->id);
-    return true;
-}
-
-inline static bool_t spinlock_unlock(spinlock_t * spinlock)
-{
-    if (!spinlock)
-    {
-        return false;
-    }
-    spinlock->counter--;
-    if (!spinlock->counter)
-    {
-        spinlock->owner = MAX_THREADS;
-        atomic_inc(&spinlock->dequeue);
-    }
-    thread_t * current_thread = kernel_get_current_thread();
-    dbg_print("[%d] Unlock spinlock...\n", current_thread->id);
-    return true;
-}
-
 typedef struct spinlock_irq_save_t
 {
     /// Internal queue.
@@ -101,67 +33,18 @@ typedef struct spinlock_irq_save_t
     uint8_t flags;
 } spinlock_irq_save_t;
 
-inline static bool_t spinlock_irq_save_init(spinlock_irq_save_t * spinlock)
-{
-    if (!spinlock)
-    {
-        return false;
-    }
-    atomic_set(&spinlock->queue, 0);
-    atomic_set(&spinlock->dequeue, 1);
-    spinlock->flags = 0;
-    spinlock->counter = 0;
-    return true;
-}
+bool_t spinlock_init(spinlock_t * spinlock);
 
-inline static bool_t spinlock_irq_save_destroy(spinlock_irq_save_t * spinlock)
-{
-    if (!spinlock)
-    {
-        return false;
-    }
-    spinlock->flags = 0;
-    spinlock->counter = 0;
-    return true;
-}
+bool_t spinlock_destroy(spinlock_t * spinlock);
 
-inline static bool_t spinlock_irq_save_lock(spinlock_irq_save_t * spinlock)
-{
-    if (!spinlock)
-    {
-        return false;
-    }
-    int32_t ticket;
-    uint8_t flags = irq_nested_disable();
-    if (spinlock->counter == 1)
-    {
-        spinlock->counter++;
-        return true;
-    }
-    ticket = atomic_add(&spinlock->queue, 1);
-    while (atomic_read(&spinlock->dequeue) != ticket)
-    {
-        __asm__("nop");
-    }
-    spinlock->flags = flags;
-    spinlock->counter = 1;
-    return true;
-}
+bool_t spinlock_lock(spinlock_t * spinlock);
 
-inline static int spinlock_irq_save_unlock(spinlock_irq_save_t * spinlock)
-{
-    if (!spinlock)
-    {
-        return false;
-    }
-    uint8_t flags;
-    spinlock->counter--;
-    if (!spinlock->counter)
-    {
-        flags = spinlock->flags;
-        spinlock->flags = 0;
-        atomic_inc(&spinlock->dequeue);
-        irq_nested_enable(flags);
-    }
-    return 0;
-}
+bool_t spinlock_unlock(spinlock_t * spinlock);
+
+bool_t spinlock_irq_save_init(spinlock_irq_save_t * spinlock);
+
+bool_t spinlock_irq_save_destroy(spinlock_irq_save_t * spinlock);
+
+bool_t spinlock_irq_save_lock(spinlock_irq_save_t * spinlock);
+
+bool_t spinlock_irq_save_unlock(spinlock_irq_save_t * spinlock);
--- src/dreamos/inc/libc/stdatomic.h
+++ src/dreamos/inc/libc/stdatomic.h
@@ -28,13 +28,13 @@ inline static void atomic_set(atomic_t * v, int32_t i)
 }
 
 /// @brief Atomically read the integer value of v.
-inline static int atomic_read(const atomic_t * v)
+inline static int32_t atomic_read(const atomic_t * v)
 {
     return READ_ONCE((v)->counter);
 }
 
 /// Atomically add i to v
-int32_t atomic_add(atomic_t * v, int32_t i)
+inline static int32_t atomic_add(atomic_t * v, int32_t i)
 {
     int result = i;
     asm volatile(LOCK_PREFIX "xaddl %0, %1" : "=r"(i) : "m"(v->counter), "0"(i) : "memory", "cc");
@@ -42,47 +42,47 @@ int32_t atomic_add(atomic_t * v, int32_t i)
 }
 
 /// Atomically subtract i from v.
-int32_t atomic_sub(atomic_t * v, int32_t i)
+inline static int32_t atomic_sub(atomic_t * v, int32_t i)
 {
     return atomic_add(v, -i);
 }
 
 /// Atomically add one to v
-int32_t atomic_inc(atomic_t * v)
+inline static int32_t atomic_inc(atomic_t * v)
 {
     return atomic_add(v, 1);
 }
 
 /// Atomically subtract one from v
-int32_t atomic_dec(atomic_t * v)
+inline static int32_t atomic_dec(atomic_t * v)
 {
     return atomic_add(v, -1);
 }
 
 /// @brief Atomically add i to v and return true if the result is negative;
 /// otherwise false.
-bool_t atomic_add_negative(atomic_t * v, int32_t i)
+inline static bool_t atomic_add_negative(atomic_t * v, int32_t i)
 {
     return (bool_t) (atomic_add(v, i) < 0);
 }
 
 /// @brief Atomically subtract i from v and return true if the result is
 /// zero; otherwise false.
-bool_t atomic_sub_and_test(atomic_t * v, int32_t i)
+inline static bool_t atomic_sub_and_test(atomic_t * v, int32_t i)
 {
     return (bool_t) (atomic_sub(v, i) == 0);
 }
 
 /// Atomically increment v by one and return true if the result is zero;
 /// false otherwise.
-int atomic_inc_and_test(atomic_t * v)
+inline static int32_t atomic_inc_and_test(atomic_t * v)
 {
     return (bool_t) (atomic_inc(v) == 0);
 }
 
 /// @brief Atomically decrement v by one and return true if zero; false
 /// otherwise.
-int atomic_dec_and_test(atomic_t * v)
+inline static int32_t atomic_dec_and_test(atomic_t * v)
 {
     return (bool_t) (atomic_dec(v) == 0);
 }
