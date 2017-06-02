0bd6afbfa6c3b96165b692ae2f12c14b818a0d56
Enrico Fraccaroli <enry.frak@gmail.com>
2017-06-02 08:44:56 +0200

    Add comments to the io files

--- src/dreamos/inc/io/video.h
+++ src/dreamos/inc/io/video.h
@@ -1,26 +1,19 @@
-/***************************************************************************
- *            video.h
- *
- *  Sat Mar 31 07:57:13 2007
- *  Copyright  2007  shainer & ^Inuyasha^
- *  Video functions and costants
- ****************************************************************************/
-
-/*
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
+/// @file   video.h
+/// @brief  Video functions and costants.
+/// @author shainer <shainer@debianclan.org> & Ivan Gualandri
+/// @date   Mar 31 2007
+/// @copyright
+/// This program is free software; you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation; either version 2 of the License, or
+/// (at your option) any later version.
+/// This program is distributed in the hope that it will be useful, but
+/// WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+/// You should have received a copy of the GNU General Public License
+/// along with this program; if not, write to the Free Software Foundation,
+/// Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #pragma once
 
--- src/dreamos/src/io/video.c
+++ src/dreamos/src/io/video.c
@@ -1,29 +1,22 @@
-/***************************************************************************
- *            video.c
- *
- *  Sat Mar 31 07:47:55 2007
- *  Copyright  2007  sgurtz
- *  Email
- *  Scrolling features and hexadecimal support added by shainer
- *  video_get_line() and video_print_ok developed by ^Inuyasha^
- *  Thanks to Schizzobau
- ****************************************************************************/
-
-/*
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
+/// @file   video.h
+/// @brief  Video functions and costants.
+/// @details Scrolling features and hexadecimal support added by shainer
+/// video_get_line() and video_print_ok developed by ^Inuyasha^
+/// Thanks to Schizzobau
+/// @author shainer <shainer@debianclan.org> & Ivan Gualandri & sgurtz
+/// @date   Mar 31 2007
+/// @copyright
+/// This program is free software; you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation; either version 2 of the License, or
+/// (at your option) any later version.
+/// This program is distributed in the hope that it will be useful, but
+/// WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+/// You should have received a copy of the GNU General Public License
+/// along with this program; if not, write to the Free Software Foundation,
+/// Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "video.h"
 #include "debug.h"
