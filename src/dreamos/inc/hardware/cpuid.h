5196db59c9bd4f93c46e9af92f13e6c5d82e0408
Enrico Fraccaroli <enry.frak@gmail.com>
2017-06-02 08:40:13 +0200

    Complete the comments for the files concerning the Hardware

--- src/dreamos/inc/hardware/cpuid.h
+++ src/dreamos/inc/hardware/cpuid.h
@@ -1,25 +1,19 @@
-/*
- * Dreamos
- * cpuid.h
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc.,51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
-/* CPUID definitions
- * Copyright by shainer <shainer@debianclan.org>
- * June 2007
- */
+/// @file   cpuid.h
+/// @brief  CPUID definitions.
+/// @author shainer <shainer@debianclan.org>
+/// @date   Jun 2007
+/// @copyright
+/// This program is free software; you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation; either version 2 of the License, or
+/// (at your option) any later version.
+/// This program is distributed in the hope that it will be useful, but
+/// WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+/// You should have received a copy of the GNU General Public License
+/// along with this program; if not, write to the Free Software Foundation,
+/// Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #pragma once
 
@@ -30,6 +24,7 @@
 #define ECX_FLAGS_SIZE 24
 #define EDX_FLAGS_SIZE 32
 
+/// @brief Contains the information concerning the CPU.
 typedef struct cpuinfo_t
 {
     char cpu_vendor[13];
--- src/dreamos/inc/hardware/timer.h
+++ src/dreamos/inc/hardware/timer.h
@@ -1,20 +1,19 @@
-/*
- * Dreamos
- * 8253.h
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
+/// @file   timer.h
+/// @brief  Timer definitions.
+/// @author shainer <shainer@debianclan.org>
+/// @date   Jun 2007
+/// @copyright
+/// This program is free software; you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation; either version 2 of the License, or
+/// (at your option) any later version.
+/// This program is distributed in the hope that it will be useful, but
+/// WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+/// You should have received a copy of the GNU General Public License
+/// along with this program; if not, write to the Free Software Foundation,
+/// Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #pragma once
 
@@ -33,4 +32,6 @@ void timer_handler();
 /// @brief Sets up the system clock by installing the timer handler into IRQ0.
 void timer_install();
 
-void sleep(const unsigned int);
+/// @brief Makes the process sleep for the given ammount of time.
+/// @param seconds The ammount of seconds.
+void sleep(const unsigned int seconds);
