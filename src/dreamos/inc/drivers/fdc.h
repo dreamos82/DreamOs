15a9e5d921611c073028e3ec8f21e03256fe08ae
Galfurian <enry.frak@gmail.com>
2017-06-05 17:19:17 +0200

    Comment floppy related code

--- src/dreamos/inc/drivers/fdc.h
+++ src/dreamos/inc/drivers/fdc.h
@@ -17,19 +17,40 @@
 
 #pragma once
 
-enum FdcRegisters
+/// @brief Floppy Disk Controller (FDC) registers.
+typedef enum fdc_registers_t
 {
-    STATUS_REGISTER_A = 0x3F0, // read-only
-    STATUS_REGISTER_B = 0x3F1, // read-only
-    DOR = 0x3F2,
-    TAPE_DRIVE_REGISTER = 0x3F3,
-    MAIN_STATUS_REGISTER = 0x3F4, // read-only
-    DATARATE_SELECT_REGISTER = 0x3F4, // write-only
-    DATA_FIFO = 0x3F5,
-    DIGITAL_INPUT_REGISTER = 0x3F7, // read-only
-    CONFIGURATION_CONTROL_REGISTER = 0x3F7  // write-only
-};
+    STATUS_REGISTER_A = 0x3F0,
+    ///< @brief This register is read-only and monitors the state of the
+    ///< interrupt pin and several disk interface pins.
+        STATUS_REGISTER_B = 0x3F1,
+    ///< @brief This register is read-only and monitors the state of several
+    ///< disk interface pins.
+        DOR = 0x3F2,
+    ///< @brief The Digital Output Register contains the drive select and
+    ///< motor enable bits, a reset bit and a DMA GATE bit.
+        TAPE_DRIVE_REGISTER = 0x3F3,
+    ///< @brief This register allows the user to assign tape support to a
+    ///< particular drive during initialization.
+        MAIN_STATUS_REGISTER = 0x3F4,
+    ///< @brief The Main Status Register is a read-only register and is used
+    ///< for controlling command input and result output for all commands.
+        DATARATE_SELECT_REGISTER = 0x3F4,
+    ///< @brief This register is included for compatibility with the 82072
+    ///< floppy controller and is write-only.
+        DATA_FIFO = 0x3F5,
+    ///< @brief All command parameter information and disk data transfers go
+    ///< through the FIFO.
+        DIGITAL_INPUT_REGISTER = 0x3F7,
+    ///< @brief This register is read only in all modes.
+        CONFIGURATION_CONTROL_REGISTER = 0x3F7
+    ///< @brief This register sets the datarate and is write only.
+} fdc_registers_t;
 
-void fdc_disable_motor(void);
-
+/// @brief Allows to enable the motor.
+/// @details The motor enable pins are directly controlled via the DOR and
+/// are a function of the mapping based on BOOTSEL bits in the TDR.
 void fdc_enable_motor(void);
+
+/// @brief Allows to disable the motor.
+void fdc_disable_motor(void);
